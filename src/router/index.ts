484596c75468e364ed4f86658edfb705d9b6f66b
mikezheng <liwenzheng.mike@gmail.com>
2022-03-21 17:25:22 +0800

    [add] router

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+// router/index.ts
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/pages/Home.vue";
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("@/pages/HelloWorld.vue"),
+  },
+];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+// vite.config.ts
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+const path = require("path");
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
